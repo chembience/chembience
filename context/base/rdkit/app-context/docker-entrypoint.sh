418cb6cf2dda41081c603f8da096b66587b94ce4
Markus Sitzmann <markus.sitzmann@gmail.com>
2018-04-06 01:02:11 +0200

    new convenience functions; addition of share

--- context/base/rdkit/app-context/docker-entrypoint.sh
+++ context/base/rdkit/app-context/docker-entrypoint.sh
@@ -3,8 +3,9 @@
 #echo "Starting RDKit app with UID $CHEMBIENCE_UID : GID $CHEMBIENCE_GID"
 
 groupadd -g $CHEMBIENCE_GID $CHEMBIENCE_USER && \
-    useradd --shell /bin/bash -u $CHEMBIENCE_UID -g $CHEMBIENCE_GID -o -c "" -M $CHEMBIENCE_USER && \
-    export HOME=$CHEMBIENCE_HOME
+    useradd --shell /bin/bash -u $CHEMBIENCE_UID -g $CHEMBIENCE_GID -o -c "" -M $CHEMBIENCE_USER
+
+export PYTHONPATH=/home/app:/opt/share:$PYTHONPATH
 
 gosu chembience "$@"
 
