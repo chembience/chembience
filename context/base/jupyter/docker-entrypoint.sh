dcf90f16fc795e4a8a689b40ba3459f6409eed38
Markus Sitzmann <markus.sitzmann@gmail.com>
2018-07-10 22:39:40 +0200

    clean up; preparing release 0.2.2

--- context/base/jupyter/docker-entrypoint.sh
+++ context/base/jupyter/docker-entrypoint.sh
@@ -19,8 +19,6 @@ if [ -z "$(ls -A /home/app)" ]; then
     mv /home/app/app-context/* /home/app/
     mv /home/app/env /home/app/.env
     rm -rf /home/app/docker-compose.init.yml /home/app/app-context
-    #mkdir -p /home/app/.local/share/jupyter/kernels
-    #cp -r /root/.local/share/jupyter/kernels/* /home/app/.local/share/jupyter/kernels
     mkdir -p $JUPYTER_NOTEBOOK_DIR
     chown -R $CHEMBIENCE_UID:$CHEMBIENCE_GID /home/app
     echo "Done."
