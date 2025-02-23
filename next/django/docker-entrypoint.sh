03d29ecd95f378cb72407d37a0934d84cec326f9
Markus Sitzmann <markus.sitzmann@gmail.com>
2025-02-23 15:37:07 +0100

    reworking postgres

--- next/django/docker-entrypoint.sh
+++ next/django/docker-entrypoint.sh
@@ -6,18 +6,29 @@ if ! id "app" >/dev/null 2>&1; then
     useradd --shell /bin/bash -u "$CHEMBIENCE_UID" -g "$CHEMBIENCE_GID" -o -c "" -M app
 fi
 
+if [ ! -d /home/app/appsite ]; then
+  echo "Creating appsite directory ..."
+  cd /home/app
+  echo 'bla'
+  django-admin startproject appsite --template=/home/template &&
+  mkdir backup
+  chown -R app:app /home/app
+  echo 'bla'
+fi
 
+cd /home/app/appsite
 
-mkdir -p /home/app
-
-chown -R app:app /home/app
-cd /home/app
-#gosu app source activate chembience
-gosu app django-admin startproject appsite --template=/home/template
-gosu app mkdir -p backup
-
-#chown -R app.app appsite backup
-echo `whoami`
+#mkdir -p /home/app
+#
+##chown -R app:app /home/app
+#cd /home/app
+##gosu app source activate chembience
+#gosu app django-admin startproject appsite --template=/home/template
+#gosu app mkdir -p backup
+#
+#cd /home/app/appsite
+##chown -R app.app appsite backup
+##echo `whoami`
 
 exec "$@"
 
